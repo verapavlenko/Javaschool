b60568f2879c3b60f6598bd42207c634af4a94cb
verapavlenko <panoramagood@gmail.com>
2017-08-19 15:31:20 +0300

    Задачи про массивы

---
+++ Lesson/src/main/java/Task1.java
@@ -0,0 +1,6 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int a = 15, b = 19;
+        System.out.println(a + b);
+    }
+}
---
+++ Lesson/src/main/java/task2.java
@@ -0,0 +1,11 @@
+public class task2 {
+    public static void main(String[] args) {
+        int n = 4;
+        int[] arr = {15, 20, 30, 40};
+        int sum = 0;
+        for (int i = 0; i <= 3; i++){
+            sum = sum + arr[i];
+        }
+        System.out.println(sum);
+    }
+}
